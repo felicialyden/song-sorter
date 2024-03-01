e0d10ca73c7d11369021743bf1067a06b164f1c2
Felicia Lydén <felicia.lyden@gmail.com>
2024-03-01 14:38:52 +0100

    display resuts in a table

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import Results from "@/components/Results"
+import './results.scss'
 
 const page = () => {
     const storedRanking = JSON.parse(localStorage.getItem('ranking') as string)
 
   return (
-    <div>
+    <div className="columns is-flex is-justify-content-center">
         <Results ranking={storedRanking}/>
     </div>
   )
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
-
 type ResultsProps = {
-    ranking: string[]
-}
+  ranking: string[];
+};
 
-const Results = ({ranking}: ResultsProps) => {
+const Results = ({ ranking }: ResultsProps) => {
   return (
-    <ul>
-        {ranking.map((song, index) => <li key={index}>{song}</li>)}
-    </ul>
-  )
-}
+    <div className="column is-half mt-6">
+    <div className="card">
+      <div className="card-content">
+        <table className="table">
+          {ranking.map((song, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{song}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </div>
+    </div>
+  );
+};
 
-export default Results
+export default Results;
