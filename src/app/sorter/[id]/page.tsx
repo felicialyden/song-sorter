f0fdfb96a292aa25fe76b264f57c7227597572ca
Felicia Lydén <felicia.lyden@gmail.com>
2024-03-01 13:54:37 +0100

    separate ranking update into its own function

--- src/app/sorter/[id]/page.tsx
+++ src/app/sorter/[id]/page.tsx
@@ -12,29 +12,40 @@ const Page = () => {
   const handleChoice = (choice: number) => {
     console.log(choice);
 
-    if(!currentRanking[currentIndexes[0] + indexDiff] && !currentRanking[currentIndexes[1] + indexDiff]) {
-      setCurrentIndexes([0 , 2])
-      setIndexDiff(indexDiff * 2)
-    } else if (!currentRanking[currentIndexes[0] + indexDiff] && currentRanking[currentIndexes[1] + indexDiff]) {
-      setCurrentIndexes([0 , 2])
+    if (
+      !currentRanking[currentIndexes[0] + indexDiff] &&
+      !currentRanking[currentIndexes[1] + indexDiff]
+    ) {
+      setCurrentIndexes([0, 0 + indexDiff / 2]);
+      setIndexDiff(indexDiff * 2);
+    } else if (
+      !currentRanking[currentIndexes[0] + indexDiff] &&
+      currentRanking[currentIndexes[1] + indexDiff]
+    ) {
+      setCurrentIndexes([0, 0 + indexDiff / 2]);
     } else if (!currentRanking[currentIndexes[1] + indexDiff]) {
-      setCurrentIndexes([currentIndexes[0] + indexDiff , 0])
-      setIndexDiff(indexDiff * 2)
+      setCurrentIndexes([currentIndexes[0] + indexDiff, 0]);
+      setIndexDiff(indexDiff * 2);
     } else {
-      setCurrentIndexes([currentIndexes[0] + indexDiff , currentIndexes[1] + indexDiff])
+      setCurrentIndexes([
+        currentIndexes[0] + indexDiff,
+        currentIndexes[1] + indexDiff
+      ]);
     }
+    updateRanking(choice)
 
-    if (choice === currentIndexes[0]) {
-      return
-    } else {
-      const songToMove = currentRanking[choice];
-      const newRanking = [...currentRanking];
-      newRanking.splice(choice, 1);
-      newRanking.splice(choice - 1, 0, songToMove);
-      setCurrentRanking(newRanking);
-    }
     console.log(currentRanking);
   };
+
+  const updateRanking = (choice: number) => {
+    if (choice === currentIndexes[0]) return;
+
+    const songToMove = currentRanking[choice];
+    const newRanking = [...currentRanking];
+    newRanking.splice(choice, 1);
+    newRanking.splice(choice - 1, 0, songToMove);
+    setCurrentRanking(newRanking);
+  }
 
   return (
     <div className="container mt-6 has-text-centered">
